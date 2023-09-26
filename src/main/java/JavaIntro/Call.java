7663c443f6cacf6ae75203ee8d64d7cb541709ab
Juanma Domínguez <1829987+juanma-dominguez@users.noreply.github.com>
2023-09-26 17:23:31 +0200

    class call api

---
+++ src/main/java/JavaIntro/Call.java
@@ -0,0 +1,26 @@
+package JavaIntro;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpClient;
+import java.io.IOException;
+public class Call {
+    public static void main(String[] args){
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://swapi.dev/api/planets/"))
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+
+        HttpResponse<String> response = null;
+
+        try {
+            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(response.body());
+
+    }
+}
